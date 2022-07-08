1af607b3e8fb68794392061b67ac0d940f218789
Sesery <ksesery1@gmail.com>
2022-07-08 12:11:08 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print alphabet  10 times n lower case
+*
+* Return; 0
+*/
+void print_alhabet_x1o(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(C);
+		}
+		_putchar('\n'); :
+	}
+}
