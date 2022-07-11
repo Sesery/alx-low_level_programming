c5299039d90c79ce1c8e79120cfbf79efb0bc5e6
Sesery <ksesery1@gmail.com>
2022-07-11 07:07:36 -0700

    98 digits

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter
  *and updates the value it points to to 98.
- *@n; number
+ *@n: pointer to n to be updated
  */
 
 void reset_to_98(int *n)
