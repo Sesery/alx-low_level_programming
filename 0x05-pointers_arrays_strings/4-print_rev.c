54e9b81d5d9ac7f96690c5c78bb775169655efb6
Sesery <ksesery1@gmail.com>
2022-07-11 13:41:43 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,12 +14,13 @@ void print_rev(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-	i++
+	i++;
 	{
 
-	for (i = length - 1; i >= 0; i--)
-	{
+	for (i = i - 1; i >= 0; i--)
+	}
 		_putchar(s[i]);
 	}
+
 	_putchar('\n');
 }
